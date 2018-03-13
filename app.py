332f97f8259ef9b14768037e46a3fa154b198d1e
John <john_huynh1@hotmail.com>
2018-03-13 00:28:24 -0400

    fixed app config

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/cs490'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/cs490'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
